2cf6a57b95acb053867e7fa03d8632b5e8b408c7
ImprimirComandas <impressaocomandas@gmail.com>
2025-05-01 01:41:54 -0300

    ha

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'domluiz.com',
+  appName: 'Dom Luiz Bebidas',
+  webDir: 'dist'
+};
+
+export default config;
